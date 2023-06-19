9c0d4645baa7d1d44f73b4067860c2caa4f2cb5e
henrique pires <henrique.pires@ntconsult.com.br>
2023-06-19 11:50:16 -0300

    projeto crud auth

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -3,6 +3,7 @@ import { AuthService } from './../../../modules/auth/services/auth.service';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { OAuthService } from 'angular-oauth2-oidc';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-header',
@@ -15,11 +16,12 @@ export class HeaderComponent {
 
   username?: string
 
-  constructor(private oauthService: OAuthService){
+  constructor(private oauthService: OAuthService, private router: Router){
     this.username = JSON.parse(localStorage.getItem('id_token_claims_obj')!).name
   }
 
   logoff(){
-    this.oauthService.logOut();
+    this.oauthService.logOut(true);
+    this.router.navigate(['/login']);
   }
 }
